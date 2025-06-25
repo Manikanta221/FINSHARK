4a44948a818ce50b38eedffbc868c12f36293a14
Manikanta221 <manikanta.tangirala24@gmail.com>
2025-06-24 22:50:24 -0500

    Initial commit

--- frontend/src/Components/CardList/CardList.tsx
+++ frontend/src/Components/CardList/CardList.tsx
@@ -1,9 +1,17 @@
+<<<<<<< HEAD
 import React, { JSX } from 'react'
+=======
+import React from 'react'
+>>>>>>> 98d14a0a (Initial commit)
 import Card from '../Card/Card'
 
 interface Props {}
 
+<<<<<<< HEAD
 const CardList : React.FC<Props> = (props: Props): JSX.Element=> {
+=======
+const CardList = (props: Props) => {
+>>>>>>> 98d14a0a (Initial commit)
   return (
     <div>
         <Card companyName='Apple' ticker='APPL' price={100}/>
